e9e7000ce2d33f7fb9f52235645624b9b1aec066
gracg <grac@capite.de>
2022-01-28 13:24:28 -0500

    iexService new getQuotes method.

--- src/main/java/nl/capite/cunsel/services/iexService.java
+++ src/main/java/nl/capite/cunsel/services/iexService.java
@@ -1,12 +1,14 @@
 package nl.capite.cunsel.services;
 
 import nl.capite.cunsel.Helpers.iexHelper;
+import nl.capite.cunsel.models.BatchQuote;
 import nl.capite.cunsel.models.Quote;
 import nl.capite.cunsel.util;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
+import java.util.List;
 
 @Service
 @Transactional
@@ -20,5 +22,9 @@ public class iexService {
 
     public Quote getQuote(String ticker) throws IOException {
         return this.iex.getQuote(ticker);
+    }
+
+    public List<BatchQuote> getQuotes(List<String> tickers) throws IOException {
+        return this.iex.getBatchQuotes(tickers);
     }
 }
