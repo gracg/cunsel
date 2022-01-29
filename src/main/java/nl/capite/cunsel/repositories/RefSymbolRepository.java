acd3639ceced866173499b2146011c6f39396a82
gracg <grac@capite.de>
2022-01-28 23:17:21 -0500

    RefSymbolRepository new getRefSymbol method.

--- src/main/java/nl/capite/cunsel/repositories/RefSymbolRepository.java
+++ src/main/java/nl/capite/cunsel/repositories/RefSymbolRepository.java
@@ -48,4 +48,23 @@ public interface RefSymbolRepository extends CrudRepository<refSymbol, String> {
             "FROM SYMBOLS WHERE type='cs' ORDER BY symbol ASC;",nativeQuery = true)
     public List<refSymbol> getRefSymbolsForCommonStock();
 
+    @Query(value = "SELECT " +
+            "symbol," +
+            "exchange," +
+            "exchangeSuffix as exchange_suffix," +
+            "exchangeName as exchange_name," +
+            "exchangeSegment as exchange_segment," +
+            "exchangeSegmentName as exchange_segment_name," +
+            "name," +
+            "type," +
+            "iexId as iex_id," +
+            "region," +
+            "currency," +
+            "isEnabled as is_enabled," +
+            "figi," +
+            "cik," +
+            "lei " +
+            "FROM SYMBOLS WHERE symbol=?1",nativeQuery = true)
+    public refSymbol getRefSymbol(String symbol);
+
 }
