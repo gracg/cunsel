60a4cd7ff2f217d966941cb7dda71cd62d93bbe7
gracg <grac@capite.de>
2022-01-28 23:17:59 -0500

    RefSymbolController new getRefSymbol method.

--- src/main/java/nl/capite/cunsel/controllers/refSymbolsController.java
+++ src/main/java/nl/capite/cunsel/controllers/refSymbolsController.java
@@ -5,6 +5,7 @@ import nl.capite.cunsel.services.RefSymbolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -26,4 +27,10 @@ public class refSymbolsController {
     @CrossOrigin
     @GetMapping("/api/x/symbols/cs")
     public List<refSymbol> getCommonStock(){return ser.getRefSymbolsForCommonStock();}
+
+    @CrossOrigin
+    @GetMapping("/api/x/symbol/{symbol}")
+    public refSymbol getRefSymbol(@PathVariable String symbol) {
+        return  ser.getRefSymbol(symbol.toUpperCase());
+    }
 }
